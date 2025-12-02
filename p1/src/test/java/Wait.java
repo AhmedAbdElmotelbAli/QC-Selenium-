78a9e981ed72db965d6eb6f76bbcc1306064fc15
Ahmed Abdelmotelb Ali <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2025-12-02 11:12:17 -0800

    add fluent wait

--- p1/src/test/java/Wait.java
+++ p1/src/test/java/Wait.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
 
 public class Wait {
     @Test
@@ -15,6 +19,7 @@ public class Wait {
         driver.findElement(startbutton).click();
     }
 
+    @Test
     public void explicitwait() {
         WebDriver driver;
         By startbutton = By.tagName("button");
@@ -26,4 +31,20 @@ public class Wait {
         org.openqa.selenium.support.ui.WebDriverWait wait = new org.openqa.selenium.support.ui.WebDriverWait(driver, java.time.Duration.ofSeconds(10));
         wait.until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(msg));
     }
-} // Add this closing brace
+
+    @Test
+    public void fluentwait() {
+        WebDriver driver;
+        By startbutton = By.tagName("button");
+        By msg = By.cssSelector("#finish h4");
+        driver = new org.openqa.selenium.chrome.ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("http://aa-practice-test-automation.vercel.app/Pages/dynamicLoading.html");
+        driver.findElement(startbutton).click();
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(java.time.Duration.ofSeconds(10))
+                .pollingEvery(java.time.Duration.ofSeconds(2))
+                .ignoring(NoSuchElementException.class);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(msg));
+    }
+}
