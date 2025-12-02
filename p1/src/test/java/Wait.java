a3c63ade5f85512d4ffcd27dfb644131dbf70e84
Ahmed Abdelmotelb Ali <104862736+AhmedAbdElmotelbAli@users.noreply.github.com>
2025-12-02 11:26:53 -0800

    edit links

--- p1/src/test/java/Wait.java
+++ p1/src/test/java/Wait.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.NoSuchElementException;
@@ -10,41 +11,46 @@ public class Wait {
     @Test
     public void implicitwait() {
         WebDriver driver;
-        By startbutton = By.tagName("button");
-        By msg = By.cssSelector("#finish h4");
+        By click = By.id("reveal");
+        By sendtext = By.id("revealed");
         driver = new org.openqa.selenium.chrome.ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
-        driver.navigate().to("http://aa-practice-test-automation.vercel.app/Pages/dynamicLoading.html");
-        driver.findElement(startbutton).click();
+        driver.navigate().to("https://www.selenium.dev/selenium/web/dynamic.html");
+        driver.findElement(click).click();
+        driver.findElement(sendtext).sendKeys("Hello");
     }
 
     @Test
     public void explicitwait() {
         WebDriver driver;
-        By startbutton = By.tagName("button");
-        By msg = By.cssSelector("#finish h4");
+        By click = By.id("reveal");
+        By sendtext = By.id("revealed");
         driver = new org.openqa.selenium.chrome.ChromeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("http://aa-practice-test-automation.vercel.app/Pages/dynamicLoading.html");
-        driver.findElement(startbutton).click();
-        org.openqa.selenium.support.ui.WebDriverWait wait = new org.openqa.selenium.support.ui.WebDriverWait(driver, java.time.Duration.ofSeconds(10));
-        wait.until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(msg));
+        driver.navigate().to("https://www.selenium.dev/selenium/web/dynamic.html");
+        driver.findElement(click).click();
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sendtext));
+        driver.findElement(sendtext).sendKeys("Hello");
     }
 
     @Test
     public void fluentwait() {
         WebDriver driver;
-        By startbutton = By.tagName("button");
-        By msg = By.cssSelector("#finish h4");
+        By click = By.id("reveal");
+        By sendtext = By.id("revealed");
+
         driver = new org.openqa.selenium.chrome.ChromeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("http://aa-practice-test-automation.vercel.app/Pages/dynamicLoading.html");
-        driver.findElement(startbutton).click();
+        driver.navigate().to("https://www.selenium.dev/selenium/web/dynamic.html");
+        driver.findElement(click).click();
         FluentWait<WebDriver> wait = new FluentWait<>(driver)
                 .withTimeout(java.time.Duration.ofSeconds(10))
                 .pollingEvery(java.time.Duration.ofSeconds(2))
                 .ignoring(NoSuchElementException.class);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(msg));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sendtext));
+        driver.findElement(sendtext).sendKeys("Hello");
+
     }
 }
